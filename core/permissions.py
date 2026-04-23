0706294ce53dcb32db5fd6a8375bf86efe0edffe
Andresini_zX <amahcan562@fp.iesromerovargas.com>
2026-04-23 14:01:54 +0200

    bugfix: fase 4. permissions.py corregido

--- core/permissions.py
+++ core/permissions.py
@@ -5,6 +5,8 @@ class IsProfileOwner(BasePermission):
     """Permite acceder solo al perfil del usuario autenticado."""
 
     def has_object_permission(self, request, view, obj):
-        return
-        (request.user.is_authenticated and return request.user.is_authenticated and obj.user == request.user
+        return (
+            request.user.is_authenticated
+            and obj.user == request.user
+        )
 
